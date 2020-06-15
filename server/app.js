1b636117546c3401e026a5fcd84ed4c56e825db9
Ted <dqkhoa02@gmail.com>
2020-06-15 14:58:09 +0700

    [DevOps][Database] Add Sequelize with Postgres Database

--- server/app.js
+++ server/app.js
@@ -1,13 +1,16 @@
 const nconf = require('nconf');
 const server = require('./server');
+const keys = require('./keys');
+
+const {connectDatabase} = require('./api/db/db');
 const { loadSettings } = require('./config/configurationAdaptor');
 
 const appSettingsPath = process.env.APP_SETTINGS_FILE_PATH;
 
 loadSettings({ appSettingsPath })
     .then(() => {
-        // TODO Connect to DB, if any. 
-
+        // TODO Connect to DB, if any. 'postgres://user:pass@example.com:5432/dbname'
+        connectDatabase(keys.pgDatabase, keys.pgUser, keys.pgPassword, keys.pgHost);
         // Read the config property required for starting the server
         const serverOptions = {
             logSeverity: nconf.get('logSeverity'),
